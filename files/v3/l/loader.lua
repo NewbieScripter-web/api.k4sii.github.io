3b09d7de0943ab12b8bb4416984aca94485c3b4e
NewbieScripter-web <rataratalol1@gmail.com>
2024-11-09 13:50:26 -0400

    Update loader.lua

--- files/v3/l/loader.lua
+++ files/v3/l/loader.lua
@@ -1,37 +1,31 @@
 --[[
+you may run the loadstring or it will wont work...
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NewbieScripter-web/k4sii.github.io/refs/heads/main/files/v3/l/loader.lua", true))()
 #K4siiontop
-You maybe need to run this on loadstring...
-or else it will kick you.
-licensed by k4sii
+
+#4:45 AM 9/11/2024
 
 #NegativeSS
 
-
-
-
-#4:45 AM 09/11/2024.
-
-
-
+licensed by k4sii
 
 ]]
+
+
+
 local a = "This script is from K4-Hub."
 local b = "8Gb471nh37Ghg9821"
-
 
 if lrm_load_script then
     lrm_load_script(b)
     while wait(1) do end
 end
 
-
 local c = "https://raw.githubusercontent.com/NewbieScripter-web/k4sii.github.io/refs/heads/main/files/v3/l/" .. b .. ".lua"
-
 
 is_from_loader = {Mode = "fastload"}
 local d = 0.03
 
--- Función de carga rápida
 l_fastload_enabled = function(e)
     if e == "flush" then
         wait(d)
@@ -43,16 +37,16 @@ l_fastload_enabled = function(e)
             wait(0.1)
             f = loadstring(g)
         end)
-        
+
         if not h or not f then
             pcall(writefile, "K4H-err-loader-log-httpresp.txt", tostring(g))
             warn("Error while loading. Err: " .. tostring(i) .. " Check K4H-err-loader-log-httpresp.txt for more details.")
             return
         end
-        
+
         f(is_from_loader)
     end
-    
+
     if e == "rl" then
         pcall(writefile, b .. "-cache.lua", "recache required")
         wait(0.2)
@@ -60,21 +54,26 @@ l_fastload_enabled = function(e)
     end
 end
 
-
 local j
-local k, l = pcall(function()
-    j = readfile(b .. "-cache.lua")
-    if (not j) or (#j < 5) then
+local success, errorMsg = pcall(function()
+    if isfile(b .. "-cache.lua") then
+        j = readfile(b .. "-cache.lua")
+        if not j or #j < 5 then
+            warn("Cache file is empty or could not be read correctly.")
+            j = nil
+            return
+        end
+        j = loadstring(j)
+    else
+        warn("Cache file does not exist.")
         j = nil
-        return
     end
-    j = loadstring(j)
 end)
 
-if not k or not j then
+if not success or not j then
+    warn("Error loading cached script: " .. (errorMsg or "Unknown error"))
     l_fastload_enabled("flush")
     return
 end
-
 
 j(is_from_loader)
